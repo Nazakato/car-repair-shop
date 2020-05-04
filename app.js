c7f46b01c0ecd2527ccf30f19e69e2eaaf5ba303
Nazakato <kurian2@i.ua>
2020-05-04 13:29:11 +0300

    Implement logging, error handling middlewares; Add nodemon

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
 const customerRoutes = require('./api/routes/customers');
 const serviceRoutes = require('./api/routes/services');
 const orderRoutes = require('./api/routes/orders');
 
+app.use(morgan('dev'));
+
 app.use('/customers', customerRoutes);
 app.use('/services', serviceRoutes);
 app.use('/orders', orderRoutes);
+
+app.use((req, res, next) => {
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.json({
+        error: {
+            message: err.message
+        }
+    })
+})
 
 module.exports = app;
