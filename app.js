aad7a8cdf24b86b507bcac7289b2a678c79101ce
Nazakato <kurian2@i.ua>
2020-05-04 15:41:49 +0300

    Enable CORS

--- app.js
+++ app.js
@@ -8,8 +8,20 @@ const serviceRoutes = require('./api/routes/services');
 const orderRoutes = require('./api/routes/orders');
 
 app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+
+    next();
+})
 
 app.use('/customers', customerRoutes);
 app.use('/services', serviceRoutes);
