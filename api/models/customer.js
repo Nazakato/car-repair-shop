6609f74b38a1872042bb9b1143f3e59c284335fe
Nazakato <kurian2@i.ua>
2020-05-06 08:17:35 +0300

    Enable enum Individual/Company on Customer.type

--- api/models/customer.js
+++ api/models/customer.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const customerSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     name: { type: String, required: true},
-    type: { type: String, required: true},
+    type: { type: String, required: true, enum: ['Individual', 'Company']},
     description: { type: String, required: true}
 });
 
