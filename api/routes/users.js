51871577b1ee0d360cee8c50d12b4ae8be62cd6d
Nazakato <kurian2@i.ua>
2020-05-11 15:19:14 +0300

    Create UserController, add get all users method, add authentication check for delete and get all users

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,98 +1,14 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
-const endpointUrl = process.env.CURRENT_DOMAIN_URL + 'customers/';
-const User = require('../models/user');
-const jwt = require('jsonwebtoken');
 const router = express.Router();
+const UserController = require('../controllers/UserController');
+const checkAuth = require('../middlewares/check-auth')
 
+router.post('/signup', UserController.sign_up);
 
-router.post('/signup', (req, res, next) => {
-  User.findOne({email: req.body.email })
-    .exec()
-    .then(user => {
-      if (user) {
-        return res.status(409).json({
-          message: 'Email exists in system'
-        });
-      } else {
-        bcrypt.hash(req.body.password, 10, (err, hash) => {
-          if (err) {
-            return res.status(500).json({
-              error: err
-            });
-          } else {
-            const user = new User({
-              _id: mongoose.Types.ObjectId(),
-              email: req.body.email,
-              password: hash,
-            });
-            user.save()
-              .then(result => {
-                console.log(result);
-                res.status(200).json({
-                  message: 'User created'
-                });
-              })
-              .catch(err => {
-                console.log(err);
-                res.status(500).json({
-                  error: err
-                });
-              });
-          }
-        });
-      }
-    });
-});
+router.post('/login', UserController.log_in);
 
-router.post('/login', (req, res, next) => {
-  User.findOne({email: req.body.email })
-    .exec()
-    .then(user => {
-      if (!user) {
-        return res.status(401).json({
-          messasge: 'Auth failed'
-        });
-      }
+router.get('/', checkAuth, UserController.get_all_users);
 
-      bcrypt.compare(req.body.password, user.password, (err, result) => {
-        if (err || !result) {
-          return res.status(401).json({
-            messasge: 'Auth failed'
-          });
-        }
-
-        const token = jwt.sign({
-          email: user.email,
-          userId: user._id
-        },
-        process.env.JWT_KEY,
-        {
-          expiresIn: "1h"
-        });
-        return res.status(200).json({
-          message: 'Auth successful',
-          token: token
-        });
-      })
-    })
-    .catch(err => {
-      res.status(500).json({ error: err.message });
-    });
-});
-
-router.delete('/:userId', (req, res, next) => {
-  User.remove({_id: req.params.userId})
-    .exec()
-    .then(result => {
-      res.status(200).json({
-        message: 'User deleted'
-      });
-    })
-    .catch(err => {
-      res.status(500).json({ error: err.message });
-    });
-})
+router.delete('/:userId', checkAuth, UserController.delete_user);
 
 module.exports = router;
